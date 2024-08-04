130ffdd576593931cb3f5dbde5cbc9ff89cb25c6
Nirupan Joe <nirupanjoej@gmail.com>
2024-08-04 12:25:37 +0530

    Modified the navBar.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,9 +9,9 @@ import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '../assets/menuIcon.png';
 import SearchIcon from '@mui/icons-material/Search';
-import MailIcon from '@mui/icons-material/Mail';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 import SettingsOutlined from '@mui/icons-material/SettingsOutlined';
-import NotificationsIcon from '@mui/icons-material/Notifications';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
 import MoreIcon from '@mui/icons-material/MoreVert';
 import Avatar from '@mui/material/Avatar';
 
@@ -54,6 +54,12 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   },
 }));
 
+const icons = [
+  { name: 'Messages', icon: EmailOutlinedIcon, badgeContent: 4 },
+  { name: 'Settings', icon: SettingsOutlined, badgeContent: null },
+  { name: 'Notifications', icon: NotificationsOutlinedIcon, badgeContent: 17 }
+];
+
 const NavBar = ({ open, setOpen }) => {
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
 
@@ -70,9 +76,6 @@ const NavBar = ({ open, setOpen }) => {
   const handleMobileMenuOpen = (event) => {
     setMobileMoreAnchorEl(event.currentTarget);
   };
-
-  const menuId = 'primary-search-account-menu';
-
 
   const mobileMenuId = 'primary-search-account-menu-mobile';
   const renderMobileMenu = (
@@ -91,109 +94,88 @@ const NavBar = ({ open, setOpen }) => {
       open={isMobileMenuOpen}
       onClose={handleMobileMenuClose}
     >
-      <MenuItem>
-        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-          <Badge badgeContent={4} color="error">
-            <MailIcon />
-          </Badge>
-        </IconButton>
-        <p>Messages</p>
-      </MenuItem>
-      <MenuItem>
-        <IconButton
-          size="large"
-          aria-label="account of current user"
-          aria-controls="primary-search-account-menu"
-          aria-haspopup="true"
-          color="inherit"
-        >
-          <SettingsOutlined />
-        </IconButton>
-        <p>Settings</p>
-      </MenuItem>
-      <MenuItem>
-        <IconButton
-          size="large"
-          aria-label="show 17 new notifications"
-          color="inherit"
-        >
-          <Badge badgeContent={17} color="error">
-            <NotificationsIcon />
-          </Badge>
-        </IconButton>
-        <p>Notifications</p>
-      </MenuItem>
+      {icons.map(({ name, icon: Icon, badgeContent }) => (
+        <MenuItem key={name}>
+          <IconButton
+            sx={{
+              backgroundColor: "#47484D",
+              alignSelf: 'center',
+              justifyContent: 'center',
+              mx: 0.5
+            }}
+            size="small"
+            color="inherit"
+          >
+            <Badge badgeContent={badgeContent} color="primary">
+              <Icon fontSize="small" />
+            </Badge>
+          </IconButton>
+          <p style={{ marginLeft: '5px'}}>{name}</p>
+        </MenuItem>
+      ))}
     </Menu>
   );
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-        <Toolbar>
+      <Toolbar>
+        <IconButton
+          onClick={handleDrawer}
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="open drawer"
+          sx={{ mr: 2 }}
+        >
+          <img width={20} height={20} src={MenuIcon} alt="logo" />
+        </IconButton>
+        <Search>
+          <SearchIconWrapper>
+            <SearchIcon />
+          </SearchIconWrapper>
+          <StyledInputBase
+            placeholder="Search…"
+            inputProps={{ 'aria-label': 'search' }}
+          />
+        </Search>
+        <Box sx={{ flexGrow: 1 }} />
+        <Box mx={2} sx={{ display: { xs: 'none', md: 'flex' } }}>
+          {icons.map(({ name, icon: Icon, badgeContent }) => (
+            <IconButton
+              key={name}
+              sx={{
+                backgroundColor: "#47484D",
+                alignSelf: 'center',
+                justifyContent: 'center',
+                mx: 0.5
+              }}
+              size="small"
+              color="inherit"
+            >
+              <Badge badgeContent={badgeContent} color="primary">
+                <Icon fontSize="small" />
+              </Badge>
+            </IconButton>
+          ))}
+        </Box>
+        <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
           <IconButton
-            onClick={handleDrawer} 
             size="large"
-            edge="start"
+            aria-label="show more"
+            aria-controls={mobileMenuId}
+            aria-haspopup="true"
+            onClick={handleMobileMenuOpen}
             color="inherit"
-            aria-label="open drawer"
-            sx={{ mr: 2 }}
           >
-            <img width={20} height={20} src={MenuIcon} alt="logo" />
+            <MoreIcon />
           </IconButton>
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search>
-          <Box sx={{ flexGrow: 1 }} />
-          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-            <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-              <Badge badgeContent={4} color="error">
-                <MailIcon />
-              </Badge>
-            </IconButton>
-            <IconButton
-              size="large"
-              edge="end"
-              aria-label="account of current user"
-              aria-controls={menuId}
-              aria-haspopup="true"
-              color="inherit"
-            >
-              <SettingsOutlined />
-            </IconButton>
-            <IconButton
-              size="large"
-              aria-label="show 17 new notifications"
-              color="inherit"
-            >
-              <Badge badgeContent={17} color="error">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-
-          </Box>
-          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="show more"
-              aria-controls={mobileMenuId}
-              aria-haspopup="true"
-              onClick={handleMobileMenuOpen}
-              color="inherit"
-            >
-              <MoreIcon />
-            </IconButton>
-          </Box>
-          <Box sx={{ flexGrow: 0 }}>
-              <IconButton sx={{ p: 0 }}>
-                <Avatar alt="Joe" src="/assets/joe.png" />
-              </IconButton>
-          </Box>
-        </Toolbar>
+        </Box>
+        <Box sx={{ flexGrow: 0 }}>
+          <IconButton sx={{ p: 0 }}>
+            <Avatar alt="Joe" src="/assets/joe.png" />
+          </IconButton>
+        </Box>
+      </Toolbar>
       {renderMobileMenu}
     </Box>
   );
